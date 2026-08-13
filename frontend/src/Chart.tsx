62618a6a436756b8ca5e9f4675e954790b618860
BeterXie <30471270+BeterXie@users.noreply.github.com>
2026-08-13 20:07:20 +0800

    Expose accessible labels on intelligence charts

--- frontend/src/Chart.tsx
+++ frontend/src/Chart.tsx
@@ -6,14 +6,22 @@ import { CanvasRenderer } from "echarts/renderers";
 
 echarts.use([LineChart, GridComponent, LegendComponent, TooltipComponent, CanvasRenderer]);
 
-export default function IntelligenceChart({ option }: { option: object }) {
+export default function IntelligenceChart({
+  option,
+  label
+}: {
+  option: object;
+  label?: string;
+}) {
   return (
-    <ReactEChartsCore
-      echarts={echarts}
-      option={option}
-      notMerge
-      lazyUpdate
-      style={{ height: "100%" }}
-    />
+    <div className="intelligence-chart" role="img" aria-label={label}>
+      <ReactEChartsCore
+        echarts={echarts}
+        option={option}
+        notMerge
+        lazyUpdate
+        style={{ height: "100%" }}
+      />
+    </div>
   );
 }
