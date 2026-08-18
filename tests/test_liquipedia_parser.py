63fe45e598afd675a36ffc7fafd95bc56578cbc9
BeterXie <30471270+BeterXie@users.noreply.github.com>
2026-08-18 13:12:48 +0800

    Organize Liquipedia parser test imports

--- tests/test_liquipedia_parser.py
+++ tests/test_liquipedia_parser.py
@@ -2,7 +2,6 @@ from datetime import UTC, datetime
 
 from app.providers.liquipedia.parser import parse_series, parse_tournaments
 
-
 TOURNAMENTS_HTML = """
 <ul class="tournaments-list">
   <li class="tournaments-list-type">
